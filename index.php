f128cdebd0ab1e28007e2a36fbe98524b264d2df
regaloeb <loeb@regaloeb.com>
2020-02-17 18:39:31 +0100

    Maj diverses

--- index.php
+++ index.php
@@ -33,12 +33,13 @@
 				<h1>RegalTabs</h1>
 				<p>
 					La présentation de contenu par onglets revient régulièrement alors voici un petit constructeur Javascript Vanilla qui permet de la générer facilement.<br><br>
-					<a href="https://github.com/regaloeb/regal-tabs-vanilla" target="_blank">Github repository</a>
+					<a href="https://github.com/regaloeb/regal-tabs-vanilla" target="_blank" class="picto-link"><span class="picto"> <svg class="octicon octicon-mark-github v-align-middle" height="32" viewBox="0 0 16 16" version="1.1" width="32" aria-hidden="true"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"/></svg>
+</span>Github repository</a>
 				</p>
 				<h2>HTML</h2>
 				<p>
 					Conteneur principal avec classe <strong>.tabs</strong><br>
-					Attribut <strong>data-default</strong> détermine l'onglet affiché par défaut&nbsp;: "0": le premier, "1": le deuxième, etc.<br>
+					Attribut <strong>data-active</strong> détermine l'onglet affiché par défaut&nbsp;: "0": le premier, "1": le deuxième, etc.<br>
 					Avec la classe <strong>.vertical</strong> la navigation sera placée à gauche des onglets, les liens les uns en dessous des autres..<br>
 					Avec la classe <strong>.horizontal</strong> la navigation sera placée au dessus des onglets, les liens les uns à côté des autres..<br>
 					Pour les mobiles, la navigation est toujours au dessus des onglets et les liens sont placés les uns sous les autres.
@@ -47,7 +48,7 @@
 					&nbsp;&nbsp;&nbsp;&nbsp;Un enfant avec classe <strong>.tabs-cont</strong> qui va contenir les onglets.<br>
 					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Chaque onglet doit avoir la classe <strong>.tab</strong>.
 					<div class="code">
-&lt;div class="tabs vertical" data-default="0"><br>
+&lt;div class="tabs vertical" data-active="0"><br>
 &nbsp;&nbsp;&nbsp;&nbsp;&lt;nav class="tabs-nav"><br>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;a href="<strong>#onglet1</strong>" class="tabs-nav-item"><br>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Onglet 1<br>
